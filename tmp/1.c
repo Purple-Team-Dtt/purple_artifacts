2f54d0be304a7b01da891a4d5545a7d12cbc7f5e
Victor P <you@example.com>
2026-07-03 12:28:55 +0000

    a

---
+++ tmp/1.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+__declspec(dllexport) void HelloWorld()
+{
+    MessageBoxA(
+        NULL,
+        "Hello, World!",
+        "Mi DLL",
+        MB_OK | MB_ICONINFORMATION
+    );
+}
